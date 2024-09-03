2f787ab9a45f1de269d143abc8908da52399beed
scaboodles <owenj.wolff2@gmail.com>
2024-09-02 20:55:10 -0400

    Initial Commit

---
+++ BoidWindows/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BoidWindows
+//
+//  Created by Owen Wolff on 9/2/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
